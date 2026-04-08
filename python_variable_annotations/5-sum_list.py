a986c18e1793d4aeec822ccd6a37e316d396fdae
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-08 10:10:19 +0200

    ADD task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+"""
+Module that contains a function that returns the sum as a float
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function that take a list of floats and return the sum of all in floats
+    """
+    res = 0
+    for i in input_list:
+        res += i
+    return res
