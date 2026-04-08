732aa2a9b8e5682b3d05639988681c7adbe3d58d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-08 09:54:39 +0200

    ADD task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/bin/usr/env python3
+
+
+"""
+Module that transform a float into a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that take a float and transfrom it into a string
+    """
+    return str(n)
