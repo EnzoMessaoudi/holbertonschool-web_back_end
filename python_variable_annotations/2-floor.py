c36ac0a4217d108090f5e347ed956dfc5ec1a024
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-08 09:49:50 +0200

    ADD task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/bin/usr/env python3
+
+
+"""
+Module that calculat ethe floor of a float
+"""
+
+
+def floor(a: float) -> int:
+    """
+    Function that calculate the floor of a float and return an int
+    """
+    return int(a)
