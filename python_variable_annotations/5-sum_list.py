bd483b4668a36480f9879bd528b8bdaf83eff686
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-08 10:11:45 +0200

    Chnage the order of the code

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from typing import List
 
 
 """
 Module that contains a function that returns the sum as a float
 """
+
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
