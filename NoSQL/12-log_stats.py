f0e9a14ed68e01e427459b49034330c88fa7ce78
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-23 10:25:30 +0200

    ADD task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+    Modulo that provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+
+db = client["logs"]
+collection = db["nginx"]
+
+number = collection.count_documents({})
+print("{} logs".format(number))
+
+print("Methods:")
+
+meth_get = collection.count_documents({"method": "GET"})
+print(" method GET: {}".format(meth_get))
+
+meth_post = collection.count_documents({"method": "POST"})
+print(" method POST: {}".format(meth_post))
+
+meth_put = collection.count_documents({"method": "PUT"})
+print(" method PUT: {}".format(meth_put))
+
+meth_patch = collection.count_documents({"method": "PATCH"})
+print(" method PATCH: {}".format(meth_patch))
+
+meth_delete = collection.count_documents({"method": "DELETE"})
+print(" method DELETE: {}".format(meth_delete))
+
+num_doc = collection.count_documents({"method": "GET", "path": "/status"})
+print("{} status check".format(num_doc))
