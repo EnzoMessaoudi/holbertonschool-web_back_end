7bf8d176c96a85f4af81d0f85b8091c99250b4e5
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-28 11:36:43 +0200

    ADD task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+    constructor (brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+cloneCar() {
+    const Species = this.constructor[Symbol.species] || this.constructor;
+    if (this.constructor[Symbol.species]) {
+        return new Species(this._brand, this._motor, this._color);
+    }
+    return new Species();
+}
+    static get [Symbol.species]() {
+        return undefined;
+    }
+}
