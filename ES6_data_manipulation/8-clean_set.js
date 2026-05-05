83ead36d953dcf0c3eb93884ae27e77580153b24
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-05 13:46:40 +0200

    ADD task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, stratString) {
+    if (stratString === '') {
+        return '';
+    }
+    let myString = "";
+
+    for(const item of set) {
+        if (item.startsWith(stratString)) {
+            if (myString !== '') {
+                myString += '-';
+            }
+            myString += item.slice(stratString.length);
+        }
+    }
+
+    return myString;
+};
