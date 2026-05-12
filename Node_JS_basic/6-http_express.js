6c97287b078ac4859aa37d3e942329400b4f1e7a
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-12 10:50:13 +0200

    ADD task 6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.set('port', process.env.PORT || 1245);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 1245);
+
+app.get('/', (req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+module.exports = app;
