7bcb00638eec79bf866c21fe347b1f2b192f19bc
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-12 08:41:24 +0200

    ADD task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        return reject(new Error('Cannot load the database'));
+      }
+      const newFile = data.split('\n');
+      const filterFile = newFile.filter((line) => line !== '');
+
+      console.log(`Number of students: ${filterFile.length - 1}`);
+      const students = filterFile.slice(1);
+
+      const fields = {};
+      students.forEach((element) => {
+        const field = element.split(',')[3];
+        const firstname = element.split(',')[0];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+
+      Object.keys(fields).forEach((field) => {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      });
+      return resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
