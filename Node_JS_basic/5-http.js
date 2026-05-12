c2bf7739caeda2d09b4f86a1941e9c1a66b8810e
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-12 10:26:20 +0200

    Put all the code in one file

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,10 +1,11 @@
 const { createServer } = require('node:http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
-const database = process.argv[2] || 'database.csv';
+const database = process.argv[2]
 
 const hostname = '127.0.0.1';
 const port = 1245;
+
 const app = createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -12,14 +13,34 @@ const app = createServer((req, res) => {
   }
   if (req.url === '/students' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    countStudents(database)
+    new Promise((resolve, reject) => {
+      if (!database) return reject(new Error('Cannot load the database'));
+      fs.readFile(database, 'utf-8', (err, data) => {
+        if (err) return reject(new Error('Cannot load the database'));
+        const filterFile = data.split('\n').filter((line) => line !== '');
+        const students = filterFile.slice(1);
+        const fields = {};
+        students.forEach((element) => {
+          const field = element.split(',')[3];
+          const firstname = element.split(',')[0];
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        });
+        const lines = [];
+        lines.push(`Number of students: ${filterFile.length - 1}`);
+        Object.keys(fields).forEach((field) => {
+          lines.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        });
+        return resolve(lines.join('\n'));
+      });
+    })
       .then((result) => {
         res.write('This is the list of our students\n');
-        res.write(`${result}`);
+        res.write(result);
         res.end();
       })
       .catch((error) => {
-        res.end(`${error.message}`);
+        res.end(error.message);
       });
   }
 });
