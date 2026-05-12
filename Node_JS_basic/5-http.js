dac20f59715c25b0b06722f1e40cf9d2e8b32c5a
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-12 09:49:23 +0200

    FIx the code with npx eslint

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,9 +1,8 @@
 const { createServer } = require('node:http');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-
 const app = createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -11,33 +10,11 @@ const app = createServer((req, res) => {
   }
   if (req.url === '/students' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    new Promise((resolve, reject) => {
-      fs.readFile('database.csv', 'utf-8', (err, data) => {
-        if (err) {
-          return reject(new Error('Cannot load the database'));
-        }
-        const filterFile = data.split('\n').filter((line) => line !== '');
-        const students = filterFile.slice(1);
-        const fields = {};
-
-        students.forEach((element) => {
-          const field = element.split(',')[3];
-          const firstname = element.split(',')[0];
-          if (!fields[field]) fields[field] = [];
-          fields[field].push(firstname);
-        });
-
-        const lines = [];
-        lines.push(`Number of students: ${filterFile.length - 1}`);
-        Object.keys(fields).forEach((field) => {
-          lines.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-        });
-
-        return resolve(lines.join('\n'));
-      });
-    })
+    countStudents('database.csv')
       .then((result) => {
-        res.end(result);
+        res.write('This is the list of our students\n');
+        res.write(result);
+        res.end();
       })
       .catch((error) => {
         res.end(error.message);
