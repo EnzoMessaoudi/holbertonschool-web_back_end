55ab08fdb93653ac28dc510253c6ffd14eada4e0
EnzoMessaoudi <12168@holbertonstudents.com>
2026-05-12 09:42:34 +0200

    ADD task 5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+const { createServer } = require('node:http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents("database.csv")
+    .then((result) => {
+        res.write('This is the list of our students\n');
+        res.write(result);
+        res.end();
+    })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  }
+});
+
+app.listen(port, hostname, () => {});
+
+module.exports = app;
